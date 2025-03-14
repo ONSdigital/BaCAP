fbfe8595da7312c3a643fa77e96e01747f53c441
henryjameslau <2945099+henryjameslau@users.noreply.github.com>
2025-03-14 10:42:06 +0000

    Better logging

--- src/routes/(main)/build/+page.svelte
+++ src/routes/(main)/build/+page.svelte
@@ -190,6 +190,8 @@
       start: true,
     };
 
+    console.log("list of OAs", store.properties.oa_all);
+    console.log("list of OAs compressed to higher geographies", store.properties.compressed);
     // currentTopics = [topics[0]]; // Default to population topic
   }
 
