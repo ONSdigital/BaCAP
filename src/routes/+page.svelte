2813b7c526c06b121f567d4708e942dd532033fd
henryjameslau <henry.lau@ons.gov.uk>
2025-12-05 14:48:11 +0000

    Button width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,19 @@
     Section,
     Titleblock,
     Button,
-    Cards,Card,
+    Cards,
+    Card,
     Footer,
-    Twisty
+    Twisty,
   } from "@onsvisual/svelte-components";
   import HeaderImage from "$lib/layout/HeaderImage.svelte";
   import Lede from "$lib/layout/Lede.svelte";
   import { base } from "$app/paths";
-  import { isDatasetAvailableInVersion,getDatasetForVersion } from "$lib/util/topic-functions";
-  import {version} from "$lib/stores/mapstore.js";
+  import {
+    isDatasetAvailableInVersion,
+    getDatasetForVersion,
+  } from "$lib/util/topic-functions";
+  import { version } from "$lib/stores/mapstore.js";
   import { get } from "svelte/store";
 
   import topicsAll from "$lib/config/topics.json";
@@ -23,20 +27,21 @@
   <div class="flex-grow">
     <Theme theme="dark">
       <Titleblock title="Build a custom area profile" background="#206095">
-        <HeaderImage/>
+        <HeaderImage />
         <Lede
           >Create your own profile for local areas with data for England and
           Wales</Lede
         >
 
         <div style:height="32px" />
-        <Button icon="arrow" iconPosition="after" href="{base}/draw"
-          >Get started</Button
-        >
-        
+        <Section width="full">
+          <Button icon="arrow" iconPosition="after" href="{base}/draw"
+            >Get started</Button
+          >
+        </Section>
       </Titleblock>
-      </Theme>
-      <Theme theme="light">
+    </Theme>
+    <Theme theme="light">
       <Section marginTop>
         <h2>How to use the profile builder</h2>
 
@@ -54,53 +59,64 @@
           able to build your profile. You will be able to download data and
           charts from your profile.
         </p> -->
-        </Section>
-        <!-- <Section width="wide" background="#f5f5f5"> -->
-          <Cards>
-            <Card title="Step 1 - Select an area">
-              <p>There are four ways to select an area:</p>
+      </Section>
+      <!-- <Section width="wide" background="#f5f5f5"> -->
+      <Cards>
+        <Card title="Step 1 - Select an area">
+          <p>There are four ways to select an area:</p>
           <ol>
             <li>Search for an area in the search box.</li>
             <li>Draw a polygon on the map.</li>
             <li>Draw a circle on the map.</li>
             <li>Upload an area boundary.</li>
           </ol>
-          <p>Once you have finished selecting an area, click on the “Build profile” button.</p>
-        </Card>
-  
-            <Card title="Step 2 - Build a profile for the area">
           <p>
-            Once you have selected your area, choose which datasets you want for your
-            profile.
+            Once you have finished selecting an area, click on the “Build
+            profile” button.
           </p>
-  
+        </Card>
+
+        <Card title="Step 2 - Build a profile for the area">
           <p>
-            The profile will be built as you choose datasets showing values for your area and a preset comparison area for context. You can change this comparison area. 
+            Once you have selected your area, choose which datasets you want for
+            your profile.
+          </p>
+
+          <p>
+            The profile will be built as you choose datasets showing values for
+            your area and a preset comparison area for context. You can change
+            this comparison area.
           </p>
 
           <p>You will be able to download data and charts from your profile.</p>
         </Card>
       </Cards>
-        <!-- </Section> -->
-        <Section width="wide">
-          
-        
-        
-
+      <!-- </Section> -->
+      <Section width="wide">
         <h3>Which datasets are available?</h3>
-        <p>This is a list of the datasets and topics available through this tool:</p>
+        <p>
+          This is a list of the datasets and topics available through this tool:
+        </p>
         <Twisty title="Census 2021 topics">
           <ul class="list-container">
-            {#each topicsAll.filter(d => isDatasetAvailableInVersion(d, get(version))).filter(t=>t.census==true).map(t=>t.label).sort((a,b)=>a.localeCompare(b)) as label}
+            {#each topicsAll
+              .filter((d) => isDatasetAvailableInVersion(d, get(version)))
+              .filter((t) => t.census == true)
+              .map((t) => t.label)
+              .sort((a, b) => a.localeCompare(b)) as label}
               <li class="list-item">{label}</li>
             {/each}
-        </ul>
+          </ul>
         </Twisty>
         <div style="height: 16px"></div>
-        
+
         <Twisty title="Non-Census datasets">
           <ul class="list-container">
-            {#each topicsAll.filter(d => isDatasetAvailableInVersion(d, get(version))).filter(t=>t.census==false).map(t=>t.label).sort((a,b)=>a.localeCompare(b)) as label}
+            {#each topicsAll
+              .filter((d) => isDatasetAvailableInVersion(d, get(version)))
+              .filter((t) => t.census == false)
+              .map((t) => t.label)
+              .sort((a, b) => a.localeCompare(b)) as label}
               <li class="list-item">{label}</li>
             {/each}
           </ul>
@@ -109,36 +125,60 @@
       </Section>
       <Section>
         <h3>How does this tool produce these figures?</h3>
-          <p>The tool takes your chosen shape and compares it to your selected datasets.</p>
-          <p>It produces a best-fit shape, which is the closest available to your chosen shape. It adds up the small area data for your best-fit shape and returns it as an estimated total.</p>
-          <p>Census 2021 topics and non-Census datasets use different small area types.</p>
-          <p>Census 2021 topics use Output Areas (OAs) whereas non-Census datasets primarily use larger Lower layer Super Output Areas (LSOAs).</p>
-          <p>We advise caution when comparing values between Census topics and non-Census datasets because these best-fit shapes will have different boundaries.</p>
+        <p>
+          The tool takes your chosen shape and compares it to your selected
+          datasets.
+        </p>
+        <p>
+          It produces a best-fit shape, which is the closest available to your
+          chosen shape. It adds up the small area data for your best-fit shape
+          and returns it as an estimated total.
+        </p>
+        <p>
+          Census 2021 topics and non-Census datasets use different small area
+          types.
+        </p>
+        <p>
+          Census 2021 topics use Output Areas (OAs) whereas non-Census datasets
+          primarily use larger Lower layer Super Output Areas (LSOAs).
+        </p>
+        <p>
+          We advise caution when comparing values between Census topics and
+          non-Census datasets because these best-fit shapes will have different
+          boundaries.
+        </p>
       </Section>
       <Section>
         <h3>Share your feedback</h3>
-          <p>We value your feedback on this service. If you would like to get in touch, please email <a href="mailto:explore.local.statistics@ons.gov.uk">explore.local.statistics@ons.gov.uk</a>.</p>
+        <p>
+          We value your feedback on this service. If you would like to get in
+          touch, please email <a
+            href="mailto:explore.local.statistics@ons.gov.uk"
+            >explore.local.statistics@ons.gov.uk</a
+          >.
+        </p>
       </Section>
     </Theme>
   </div>
 </div>
 <Footer />
+
 <style>
   .list-container {
-            display: flex;
-            flex-wrap: wrap;
-            gap: 12px;
-            list-style-type: none;
-            padding: 0;
-        }
+    display: flex;
+    flex-wrap: wrap;
+    gap: 12px;
+    list-style-type: none;
+    padding: 0;
+  }
   .list-item {
-      background-color: #f8f9fa;
-      border-radius: 4px;
-      padding: 10px 15px;
-      box-shadow: 0 1px 3px rgba(0,0,0,0.1);
-      min-width: 200px;
-      flex: 1 1 auto;
-      transition: all 0.2s ease;
+    background-color: #f8f9fa;
+    border-radius: 4px;
+    padding: 10px 15px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+    min-width: 200px;
+    flex: 1 1 auto;
+    transition: all 0.2s ease;
   }
   /* .list-item:hover {
       background-color: #e9ecef;
@@ -146,13 +186,13 @@
       box-shadow: 0 3px 5px rgba(0,0,0,0.15);
   } */
   @media (max-width: 768px) {
-      .list-item {
-          min-width: 150px;
-      }
+    .list-item {
+      min-width: 150px;
+    }
   }
   @media (max-width: 480px) {
-      .list-item {
-          min-width: 100%;
-      }
+    .list-item {
+      min-width: 100%;
+    }
   }
 </style>
