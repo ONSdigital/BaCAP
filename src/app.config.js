43e9c85ad1d5a92a97ba5978e355de8ef45641d8
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2026-06-11 12:15:03 +0100

    change app config

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,5 @@
 // APP CONFIG
 
 // Base paths
-export const base_prod = '/visualisations/sveltekit-starter'; // Directory on the ONS website
-export const base_preview = '/sveltekit-starter'; // Directory on datavisweb preview server or Github Pages
-
+export const base_prod = "/visualisations/customprofiles"; // Directory on the ONS website
+export const base_preview = "/bacap-next"; // Directory on datavisweb preview server or Github Pages
