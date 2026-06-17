8d16a7230dc2a54f0bf5f4f12a95998c5d452fc6
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2026-06-17 16:31:50 +0100

    add basic meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,4 +5,12 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Build a custom area profile - ONS</title>
+	<meta
+		name="description"
+		content="Create your own profile for local areas with data for England and Wales. Data topics include population, age, sex, ethnicity, religion, the work people do, and the homes they live in."
+	/>
+</svelte:head>
+
 {@render children()}
