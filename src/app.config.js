9b177c65574f5271d3bc231817c7cfd88b53e7fb
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2026-06-17 16:25:44 +0100

    change preview path

--- src/app.config.js
+++ src/app.config.js
@@ -2,4 +2,4 @@
 
 // Base paths
 export const base_prod = "/visualisations/customprofiles"; // Directory on the ONS website
-export const base_preview = ""; // Directory on datavisweb preview server or Github Pages
+export const base_preview = "/bacap-next"; // Directory on datavisweb preview server or Github Pages
