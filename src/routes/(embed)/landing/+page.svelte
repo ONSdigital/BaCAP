875bcb69194dacbdd4b2a8c9ac2cec844f53827b
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2026-07-10 13:52:56 +0100

    add landing page embed

---
+++ src/routes/(embed)/landing/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+	import { onMount } from "svelte";
+	import { resolve } from "$app/paths";
+	import { Embed, Container, Button } from "@onsvisual/svelte-components";
+	import AreaSearch from "$lib/ui/AreaSearch.svelte";
+	import { getAreasList } from "$lib/utils.js";
+
+	let areasList = $state.raw();
+	let selectedArea = $state.raw(null);
+
+	async function init() {
+		areasList = await getAreasList(true);
+	}
+	onMount(init);
+
+	$inspect({ selectedArea });
+</script>
+
+<Embed id="landing">
+	<Container theme="paleblue">
+		<div class="ons-u-p-l">
+			<h2>Find your area</h2>
+			<p>
+				Find a ready-made area to start building your custom profile. Available areas
+				include local authorities, wards, parishes and parliamentary constituencies.
+			</p>
+			{#if areasList}
+				<form
+					onsubmit={(e) => {
+						e.preventDefault();
+						window.top.location.href = resolve(`/build/#${selectedArea.id}`);
+					}}
+				>
+					<AreaSearch
+						bind:value={selectedArea}
+						options={areasList}
+						label="Type to select"
+					/>
+					<div class="ons-u-mt-2xs">
+						<Button
+							variant="secondary"
+							disabled={!selectedArea}
+							on:click={() => {
+								window.top.location.href = resolve(`/draw/#${selectedArea.id}`);
+							}}
+							small>Edit on map</Button
+						>
+						<Button
+							type="submit"
+							icon="arrow"
+							iconPosition="after"
+							disabled={!selectedArea}
+							small>Build profile</Button
+						>
+					</div>
+				</form>
+			{/if}
+		</div>
+	</Container>
+</Embed>
